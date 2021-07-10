c4ad2a97de26fb6129913f4d41d9885a6afd4ceb
Xxpromw3mtxX <fra.metta@hotmail.it>
2021-07-10 11:52:23 +0200

    Update phoenix_drugs.sql

--- phoenix_drugs.sql
+++ phoenix_drugs.sql
@@ -2,5 +2,7 @@ USE `es_extended`;
 
 INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES
 ('cannabis', 'Cannabis', 2, 0, 1),
-('marijuana', 'Marijuana', 2, 0, 1)
+('marijuana', 'Marijuana', 2, 0, 1),
+('cima_amnesia', 'Tip of Amnesia', 2, 0, 1),
+('amnesia', 'Amnesia', 2, 0, 1)
 ;
