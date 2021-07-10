7f6cc02325f5adcb0ad20e6af0450e963714aa05
Xxpromw3mtxX <fra.metta@hotmail.it>
2021-07-10 11:52:50 +0200

    semplified and removed ambiguity

--- locales/it.lua
+++ locales/it.lua
@@ -1,13 +1,23 @@
 Locales ['it'] = {
     -- Weed
-    ['weed_processingfull'] = 'Processo cancellato a causa di spazio insufficiente',
-    ['weed_processingenough'] = 'Non hai abbastanza cannabis per processare! Devi averne almeno 3 unità.',
-    ['weed_processing'] = 'Processando...',
-    ['weed_pickinup'] = 'Raccogliendo...',
     ['weed_pickupprompt'] = 'Premi ~INPUT_CONTEXT~ per racoggliere la ~g~Cannabis~s~.',
     ['weed_processprompt'] = 'Premi ~INPUT_CONTEXT~ per processare la ~g~Cannabis~s~.',
+
+    -- Amnesia
+    ['amnesia_pickupprompt'] = 'Premi ~INPUT_CONTEXT~ per racoggliere l\'~g~Amnesia~s~.',
+    ['amnesia_processprompt'] = 'Premi ~INPUT_CONTEXT~ per processare l\'~g~Amnesia~s~.',
+
+    -- Pickup
+    ['pickup'] = 'Raccogliendo...',
+
+    -- Process
+    ['processingfull'] = 'Processo cancellato a causa di spazio insufficiente',
+    ['processingenough'] = 'Non hai abbastanza cannabis per processare! Devi averne almeno 3 unità.',
+    ['processing'] = 'Processando...',
 
     -- Blips
     ['blip_weedfield'] = 'Raccolta Maria',
     ['blip_weedprocess'] = 'Processo Maria',
+    ['blip_amnesiafield'] = 'Raccolta Amnesia',
+    ['blip_amnesiaprocess'] = 'Processo Amnesia',
 }
