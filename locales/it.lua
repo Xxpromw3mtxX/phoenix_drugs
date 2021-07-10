bfe0e3dd0025de6540605ed596800edbf1171824
Xxpromw3mtxX <fra.metta@hotmail.it>
2021-07-10 11:09:51 +0200

    it lang config file

---
+++ locales/it.lua
@@ -0,0 +1,13 @@
+Locales ['it'] = {
+    -- Weed
+    ['weed_processingfull'] = 'Processo cancellato a causa di spazio insufficiente',
+    ['weed_processingenough'] = 'Non hai abbastanza cannabis per processare! Devi averne almeno 3 unità.',
+    ['weed_processing'] = 'Processando...',
+    ['weed_pickinup'] = 'Raccogliendo...',
+    ['weed_pickupprompt'] = 'Premi ~INPUT_CONTEXT~ per racoggliere la ~g~Cannabis~s~.',
+    ['weed_processprompt'] = 'Premi ~INPUT_CONTEXT~ per processare la ~g~Cannabis~s~.',
+
+    -- Blips
+    ['blip_weedfield'] = 'Raccolta Maria',
+    ['blip_weedprocess'] = 'Processo Maria',
+}
